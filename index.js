aeec31ac6321e6f0e3c10ddc82d9b4d1986731df
hawkeyexl <hawkeyexl@gmail.com>
2022-05-03 02:47:12 -0700

    refactored arg, config, and file parsing

--- index.js
+++ index.js
@@ -8,64 +8,108 @@ const fs = require("fs");
 const { exit } = require("process");
 
 const debug = true;
-let config = require("./config.json");
-let dirs = [];
-let files = [];
+let argv = setArgs(process.argv);
+let config = setConfig(require("./config.json"), argv);
+let files = setFiles(config);
 let tests = [];
 
-// Define args
-const argv = yargs(hideBin(process.argv))
-  .option("config", {
-    alias: "c",
-    description: "Path to a custom config file",
-    type: "string",
-  })
-  .option("testFile", {
-    alias: "f",
-    description: "Path to a test",
-    type: "string",
-  })
-  .option("testDir", {
-    alias: "d",
-    description: "Path to a ditectory of tests",
-    type: "string",
-  })
-  .option("recursive", {
-    alias: "r",
-    description: "Recursively find test files in the test directory.",
-    type: "string",
-  })
-  .option("ext", {
-    alias: "e",
-    description:
-      "Comma-separated list of file extensions to test, including the leading period",
-    type: "string",
-  })
-  .option("seleniumServer", {
-    description: "Path to video output directory",
-    type: "string",
-  })
-  .option("imageDir", {
-    alias: "i",
-    description: "Path to image output directory",
-    type: "string",
-  })
-  .option("videoDir", {
-    alias: "v",
-    description: "Path to video output directory",
-    type: "string",
-  })
-  .help()
-  .alias("help", "h").argv;
+// Set array of test files
+function setFiles(config) {
+  let dirs = [];
+  let files = [];
+  if (config.testFile) {
+    // if single file specified
+    let file = path.resolve(config.testFile);
+    if (fs.statSync(file).isFile()) {
+      files[0] = file;
+    } else {
+      console.log("Error: Specified path isn't a valid file.");
+      exit(1);
+    }
+  } else {
+    // Load files from drectory
+    dirs[0] = config.testDirectory;
+    for (let i = 0; i < dirs.length; i++) {
+      fs.readdirSync(dirs[i]).forEach((object) => {
+        let content = path.resolve(dirs[i] + "/" + object);
+        if (fs.statSync(content).isFile()) {
+          // is a file
+          if (
+            // No specified extension filter list, or file extension is present in extension filter list.
+            config.testExtensions === "" ||
+            config.testExtensions.includes(path.extname(content))
+          ) {
+            files.push(content);
+          }
+        } else if (fs.statSync(content).isDirectory) {
+          // is a directory
+          if (config.recursive) {
+            // recursive set to true
+            dirs.push(content);
+          }
+        } else {
+          console.log(
+            "Error: " + content + " isn't a valid file or directory."
+          );
+          exit(1);
+        }
+      });
+    }
+  }
+  return files;
+}
 
-config = await setConfig(config, argv);
-
-console.log(config);
 exit();
 
-async function setConfig(config,argv) {
+// Define args
+function setArgs(args) {
+  let argv = yargs(hideBin(args))
+    .option("config", {
+      alias: "c",
+      description: "Path to a custom config file",
+      type: "string",
+    })
+    .option("testFile", {
+      alias: "f",
+      description: "Path to a test",
+      type: "string",
+    })
+    .option("testDir", {
+      alias: "d",
+      description: "Path to a ditectory of tests",
+      type: "string",
+    })
+    .option("recursive", {
+      alias: "r",
+      description: "Recursively find test files in the test directory.",
+      type: "string",
+    })
+    .option("ext", {
+      alias: "e",
+      description:
+        "Comma-separated list of file extensions to test, including the leading period",
+      type: "string",
+    })
+    .option("imageDir", {
+      alias: "i",
+      description: "Path to image output directory",
+      type: "string",
+    })
+    .option("videoDir", {
+      alias: "v",
+      description: "Path to video output directory",
+      type: "string",
+    })
+    .help()
+    .alias("help", "h").argv;
+
+  return argv;
+}
+
+function setConfig(config, argv) {
   // Set config overrides from args
   if (argv.config) config = JSON.parse(fs.readFileSync(argv.config));
+  if (argv.testFile) config.testFile = path.resolve(argv.testFile);
   if (argv.testDir) config.testDirectory = path.resolve(argv.testDir);
   if (argv.imageDir) config.imageDirectory = path.resolve(argv.imageDir);
   if (argv.videoDir) config.videoDirectory = path.resolve(argv.videoDir);
@@ -80,7 +124,6 @@ async function setConfig(config,argv) {
     }
   }
   if (argv.ext) config.testExtensions = argv.ext.replace(/\s+/g, "").split(",");
-  if (argv.seleniumServer) config.seleniumServer = argv.seleniumServer;
   return config;
 }
 
