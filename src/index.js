36ad2b9080295b9629de63ba87c91c8736ee1545
hawkeyexl <hawkeyexl@gmail.com>
2022-10-20 10:21:55 -0700

    Set up suggest cli command

--- src/index.js
+++ src/index.js
@@ -10,10 +10,12 @@ const { sendAnalytics } = require("./lib/analytics.js");
 const { runTests } = require("./lib/tests");
 const { checkTestCoverage, checkMarkupCoverage } = require("./lib/analysis");
 const { reportCoverage } = require("./lib/coverage");
+const { suggestTests } = require("./lib/suggest");
 const { exit } = require("process");
 
 exports.run = main;
 exports.coverage = coverage;
+exports.suggest = suggest;
 
 async function main(config, argv) {
   // Set args
@@ -67,17 +69,49 @@ async function coverage(config, argv) {
   log(config, "debug", files);
 
   const testCoverage = checkTestCoverage(config, files);
-  log(config, "debug", "(DEBUG) TEST COVERAGE:");
+  log(config, "debug", "TEST COVERAGE:");
   log(config, "debug", testCoverage);
 
   const markupCoverage = checkMarkupCoverage(config, testCoverage);
-  log(config, "debug", "(DEBUG) MARKUP COVERAGE:");
+  log(config, "debug", "MARKUP COVERAGE:");
   log(config, "debug", markupCoverage);
 
   const coverageReport = reportCoverage(config, markupCoverage);
-  log(config, "debug", "(DEBUG) COVERAGE REPORT:");
+  log(config, "debug", "COVERAGE REPORT:");
   log(config, "debug", coverageReport);
 
   // Output
   outputResults(config.coverageOutput, coverageReport, config);
+}
+
+async function suggest(config, argv) {
+  // Set args
+  argv = setArgs(argv);
+  log(config, "debug", `ARGV:`);
+  log(config, "debug", argv);
+
+  // Set config
+  config = setConfig(config, argv);
+  log(config, "debug", `CONFIG:`);
+  log(config, "debug", config);
+
+  // Set files
+  const files = setFiles(config);
+  log(config, "debug", `FILES:`);
+  log(config, "debug", files);
+
+  const testCoverage = checkTestCoverage(config, files);
+  log(config, "debug", "TEST COVERAGE:");
+  log(config, "debug", testCoverage);
+
+  const markupCoverage = checkMarkupCoverage(config, testCoverage);
+  log(config, "debug", "MARKUP COVERAGE:");
+  log(config, "debug", markupCoverage);
+
+  const testSuggestions = suggestTests(config, markupCoverage);
+  log(config, "debug", "TEST SUGGESTIONS:");
+  log(config, "debug", testSuggestions);
+
+  // Output
+  outputResults(config.testSuggestionOutput, testSuggestions, config);
 }
