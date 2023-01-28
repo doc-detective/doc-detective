0fb109bbd72e282b483a422fce1330a13663b8f3
hawkeyexl <hawkeyexl@gmail.com>
2023-01-28 11:07:42 -0800

    Electron base

---
+++ main.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  mainWindow.loadFile("index.html");
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
