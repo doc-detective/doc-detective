53952578352428d9a43439b560784b8307208346
manuelrbsilva <48723033+manuelrbsilva@users.noreply.github.com>
2023-01-28 20:02:45 -0800

    Added basic Electron CLI handling

--- main.js
+++ main.js
@@ -1,20 +1,41 @@
+const { isSafeToUnpackElectronOnRemoteBuildServer } = require("app-builder-lib/out/platformPackager");
 const { app, BrowserWindow } = require("electron");
 
 function createWindow() {
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-  });
+  let isCalledViaCLI = checkIfCalledViaCLI(process.argv);
+
+  if (isCalledViaCLI) {
+    mainWindow = new BrowserWindow({ show: false, width: 0, height: 0 });
+  } else {
+    mainWindow = new BrowserWindow({
+      width: 800,
+      height: 600,
+      show: false
+    });
+  }
 
   mainWindow.loadFile("index.html");
+  mainWindow.once('ready-to-show', () => {
+    if (isCalledViaCLI) {
+      mainWindow.hide();
+      // TODO: Add CLI handling
+    } else {
+      mainWindow.show();
+    }
+  })
+}
+
+function checkIfCalledViaCLI(args) {
+  if (args && args.length > 1 && !args[0].includes("electron.exe")) {
+    return true;
+  }
+  return false;
 }
 
 app.whenReady().then(createWindow);
 
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+  app.quit();
 });
 
 app.on("activate", () => {
