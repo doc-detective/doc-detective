551837c106f685cf25a3715197edbbece3a37974
hawkeyexl <hawkeyexl@gmail.com>
2023-08-01 14:07:27 -0700

    Test update for GitHub Actions

--- test/runTests.test.js
+++ test/runTests.test.js
@@ -1,17 +1,22 @@
 const { spawnCommand } = require("../src/utils");
 const assert = require("assert").strict;
+const fs = require("fs");
 const path = require("path");
 const artifactPath = path.resolve("./test/artifacts");
 
-describe("Run tests sucessfully", function() {
+describe("Run tests sucessfully", function () {
   // Set indefinite timeout
   this.timeout(0);
   it("All specs pass", async () => {
-    const runTests = await spawnCommand(`node ./src/runTests.js -c ${artifactPath}/config.json -i ${artifactPath}/doc-content.md -o ${artifactPath}`);
+    const runTests = await spawnCommand(
+      `node ./src/runTests.js -c ${artifactPath}/config.json -i ${artifactPath}/doc-content.md -o ${artifactPath}`
+    );
     // Find output file
     const outputFiles = runTests.stdout.split("See results at ");
     const outputFile = outputFiles[outputFiles.length - 1].trim();
-    const result = require(outputFile);
+    const result = JSON.parse(
+      fs.readFileSync(outputFile, { encoding: "utf8" })
+    );
     assert.equal(result.summary.specs.pass, 1);
   });
 });
