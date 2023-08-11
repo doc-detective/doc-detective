694d156979ac61a85b7e0ea4f1c2152d063642ef
hawkeyexl <hawkeyexl@gmail.com>
2023-08-11 16:02:43 -0700

    Added body padding and AppBar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,20 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import config_v2 from 'doc-detective-common/src/schemas/output_schemas/config_v2.schema.json';
-import checkLink_v2 from 'doc-detective-common/src/schemas/output_schemas/checkLink_v2.schema.json';
-import validator from '@rjsf/validator-ajv8';
-import Form from '@rjsf/mui';
-import FileUploader from './FileUploader';
-import TestButton from './Button';
+import AppBar from "./components/AppBar";
 
 const uiSchema = {}
 
 const log = (type) => console.log.bind(console, type);
 ReactDOM.render(
   <div>
-    <TestButton />
-    <FileUploader />
+    <AppBar />
+    <div class="body">
     <Form
       schema={checkLink_v2}
       uiSchema={uiSchema}
@@ -23,6 +15,7 @@ ReactDOM.render(
       onSubmit={log('submitted')}
       onError={log('errors')}
     />
+    </div>
   </div>,
   document.getElementById('root')
 );
