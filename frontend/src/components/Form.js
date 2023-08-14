394a590bfa27ba4856fbda9ddc327e1f670e689a
hawkeyexl <hawkeyexl@gmail.com>
2023-08-14 10:17:55 -0700

    Added various params

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,74 +1,109 @@
-import React, { useState } from 'react';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
+import React, { useState } from "react";
+import ReactMarkdown from "react-markdown";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 import checkLink_v2 from "doc-detective-common/src/schemas/output_schemas/checkLink_v2.schema.json";
+import { v4 as uuidv4 } from "uuid";
 
 const Form = () => {
+  // Temp for development
+  let schema = checkLink_v2;
 
-    const generateFormFields = (schema) => {
-        const formFields = [];
+  const generateFormFields = (schema) => {
+    const formFields = [];
 
-        for (const [key, value] of Object.entries(schema.properties)) {
-            let field;
+    for (const [key, value] of Object.entries(schema.properties)) {
+      let field;
+      let defaultValue = "";
+      let required = false;
+      let label = value.title || value.name || key;
+      let helperText = value.description || "";
+      let placeholder = ""; // TODO: Add placeholders in schema
 
-            switch (value.type) {
-                case 'string':
-                    field = <TextField label={key} />;
-                    break;
-                case 'boolean':
-                    field = <Checkbox label={key} />;
-                    break;
-                case 'integer':
-                    field = (
-                        <Select label={key}>
-                            {value.enum.map((option) => (
-                                <MenuItem value={option}>{option}</MenuItem>
-                            ))}
-                        </Select>
-                    );
-                    break;
-                default:
-                    if (value.properties) {
-                        field = generateFormFields(value);
-                    }
-                    break;
-            }
+      // Get default value
+      if (value.const) {
+        defaultValue = value.const;
+      } else if (value.default) {
+        defaultValue = value.default;
+      } else if (
+        schema.dynamicDefaults &&
+        schema.dynamicDefaults[key] &&
+        schema.dynamicDefaults[key] === "uuid"
+      ) {
+        defaultValue = uuidv4();
+      }
 
-            if (field) {
-                formFields.push(field);
-            }
-        }
+      // Check if field is required
+      if (schema.required && schema.required.includes(key)) {
+        required = true;
+      }
 
-        return formFields;
+      switch (value.type) {
+        case "string":
+          field = (
+            <TextField
+              label={label}
+              required={required}
+              helperText={helperText}
+              // placeholder={placeholder}
+              value={defaultValue}
+            />
+          );
+          break;
+        case "boolean":
+          field = <Checkbox label={key} />;
+          break;
+        case "integer":
+          field = (
+            <Select label={key}>
+              {value.enum.map((option) => (
+                <MenuItem value={option}>{option}</MenuItem>
+              ))}
+            </Select>
+          );
+          break;
+        default:
+          if (value.properties) {
+            field = generateFormFields(value);
+          }
+          break;
+      }
+
+      if (field) {
+        formFields.push(field);
+      }
     }
 
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [message, setMessage] = useState('');
-    const [fields, setFields] = useState([{ value: null }]);
+    return formFields;
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(`Name: ${name}\nEmail: ${email}\nMessage: ${message}`);
-    };
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [fields, setFields] = useState([{ value: null }]);
 
-    const handleAddField = () => {
-        const values = [...fields];
-        values.push({ value: null });
-        setFields(values);
-    };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Name: ${name}\nEmail: ${email}\nMessage: ${message}`);
+  };
 
-    const handleFieldChange = (index, event) => {
-        const values = [...fields];
-        values[index].value = event.target.value;
-        setFields(values);
-    };
+  const handleAddField = () => {
+    const values = [...fields];
+    values.push({ value: null });
+    setFields(values);
+  };
 
-    const formFields = generateFormFields(checkLink_v2);
+  const handleFieldChange = (index, event) => {
+    const values = [...fields];
+    values[index].value = event.target.value;
+    setFields(values);
+  };
 
-    return (
-        <form onSubmit={handleSubmit}>
-            {/* <TextField
+  const formFields = generateFormFields(checkLink_v2);
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {/* <TextField
                 label="Name"
                 value={name}
                 onChange={(event) => setName(event.target.value)}
@@ -91,10 +126,8 @@ const Form = () => {
                 rows={4}
                 required
             /> */}
-            {formFields.map((field) => (
-                field
-            ))}
-            {/* {fields.map((field, index) => (
+      {formFields.map((field) => field)}
+      {/* {fields.map((field, index) => (
                 <TextField
                     key={index}
                     label={`Field ${index + 1}`}
@@ -104,14 +137,14 @@ const Form = () => {
                     required
                 />
             ))} */}
-            <Button type="submit" variant="contained" color="primary">
-                Submit
-            </Button>
-            <Button onClick={handleAddField} variant="contained" color="secondary">
-                +
-            </Button>
-        </form>
-    );
+      <Button type="submit" variant="contained" color="primary">
+        Submit
+      </Button>
+      <Button onClick={handleAddField} variant="contained" color="secondary">
+        +
+      </Button>
+    </form>
+  );
 };
 
 export default Form;
