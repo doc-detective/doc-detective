9916bb7f20004f298989b9f7f3976ad2187ac226
hawkeyexl <hawkeyexl@gmail.com>
2023-08-15 10:05:13 -0700

    Disabled action field editing and added beginning array support

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,6 +1,14 @@
 import React, { useEffect, useState, useCallback } from "react";
 import ReactMarkdown from "react-markdown";
-import { FormControlLabel, Checkbox, TextField, Button } from "@mui/material";
+import {
+  Container,
+  FormControlLabel,
+  Checkbox,
+  TextField,
+  Button,
+  IconButton,
+} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
 import checkLink_v2 from "doc-detective-common/src/schemas/output_schemas/checkLink_v2.schema.json";
 import { v4 as uuidv4 } from "uuid";
 
@@ -41,6 +49,12 @@ const Form = () => {
       let label = value.title || value.name || key;
       let helperText = value.description || "";
       let placeholder = "";
+      let disabled = false;
+
+      // Get disabled
+      if (key === "action") {
+        disabled = true;
+      }
 
       // Get placeholder
       if (value.examples && value.examples.length > 0) {
@@ -59,11 +73,12 @@ const Form = () => {
               id={fieldId}
               label={label}
               required={required}
+              disabled={disabled}
               helperText={helperText}
               placeholder={placeholder}
               value={valueState[fieldId]}
               onChange={(event) =>
-                setValueState({ ...valueState, fieldId: event.target.value })
+                setValueState({ ...valueState, [fieldId]: event.target.value })
               }
             />
           );
@@ -75,11 +90,12 @@ const Form = () => {
               id={fieldId}
               label={label}
               required={required}
+              disabled={disabled}
               helperText={helperText}
               placeholder={placeholder}
               value={valueState[fieldId]}
               onChange={(event) =>
-                setValueState({ ...valueState, fieldId: event.target.value })
+                setValueState({ ...valueState, [fieldId]: event.target.value })
               }
             />
           );
@@ -106,6 +122,52 @@ const Form = () => {
         //     />
         //   );
         //   break;
+        case "array":
+          field = (
+            <Container>
+              {valueState[fieldId].map((value, index) => (
+                <div key={index}>
+                  <TextField
+                    value={value}
+                    onChange={(event) => {
+                      const newValues = [...valueState[fieldId]];
+                      newValues[index] = event.target.value;
+                      setValueState({
+                        ...valueState,
+                        [fieldId]: newValues,
+                      });
+                    }}
+                  />
+                  <IconButton
+                    aria-label="delete"
+                    onClick={() => {
+                      const newValues = [...valueState[fieldId]];
+                      newValues.splice(index, 1);
+                      setValueState({
+                        ...valueState,
+                        [fieldId]: newValues,
+                      });
+                    }}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </div>
+              ))}
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => {
+                  setValueState({
+                    ...valueState,
+                    [fieldId]: [...valueState[fieldId], ""],
+                  });
+                }}
+              >
+                Add Field
+              </Button>
+            </Container>
+          );
+          break;
         case "integer":
           field = (
             <Select label={key}>
