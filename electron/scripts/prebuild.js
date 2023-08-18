d0410f798f74ad1923e3988f11ebe66b6d34b03d
hawkeyexl <hawkeyexl@gmail.com>
2023-08-18 11:21:13 -0700

    Added initial Electron building config

---
+++ electron/scripts/prebuild.js
@@ -0,0 +1,18 @@
+const { exec } = require("child_process");
+const fs = require("fs");
+
+const frontendBuild = exec("npm run build", { cwd: "../frontend" });
+
+frontendBuild.on("exit", (code) => {
+  if (code !== 0) {
+    console.error(`Build failed with code ${code}`);
+    return;
+  }
+
+  fs.cpSync(
+    "../frontend/build",
+    "./frontend",
+    {recursive: true, force: true}
+  );
+  console.log("Build files copied successfully!");
+});
