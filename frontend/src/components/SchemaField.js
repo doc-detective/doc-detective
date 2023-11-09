8a36c34f5ec5820958c45a9bd9edc7fd493db099
hawkeyexl <hawkeyexl@gmail.com>
2023-11-09 11:18:40 -0800

    Add SchemaField component and stories

---
+++ frontend/src/components/SchemaField.js
@@ -0,0 +1,139 @@
+import React, { useState } from "react";
+import ReactMarkdown from "react-markdown";
+import { TextField } from "@mui/material";
+
+import { Switch, FormControlLabel } from "@mui/material";
+
+const SchemaField = ({
+  schema,
+  pathToKey,
+  propertyKey,
+  propertyValue,
+  setFieldData,
+}) => {
+  // Prop definitions.
+  // schema: The schema that the field belongs to.
+  // pathToKey: The path to the field in the schema.
+  // propertyKey: The key of the field.
+  // propertyValue: The property object that defines the field.
+  // setFieldData: The function that updates the field data.
+
+  // If the field is marked as const, it should not be editable.
+  if (propertyValue.const) {
+    return null;
+  }
+
+  // Run custom logic.
+  const fieldPath = pathToKey ? `${pathToKey}.${propertyKey}` : propertyKey;
+  const label = propertyValue.title || propertyValue.name || propertyKey;
+  const helperText = propertyValue.description || "";
+  const defaultValue = propertyValue.default || "";
+  const required = schema.required && schema.required.includes(propertyKey);
+  const placeholder =
+    propertyValue.examples && propertyValue.examples.length > 0
+      ? propertyValue.examples[0]
+      : null;
+  // TODO: Add validation rules
+  // TODO: Evaluate type
+
+  // Set up state.
+  const [fieldValue, setFieldValue] = useState(defaultValue);
+
+  // Handle strings
+  if (propertyValue.type === "string" && !propertyValue.enum) {
+    return (
+      <div class="field" key={fieldPath}>
+        <ReactMarkdown>{`## ${label}${required ? "*" : ""}`}</ReactMarkdown>
+        <ReactMarkdown>{helperText}</ReactMarkdown>
+        <TextField
+          key={fieldPath}
+          required={required}
+          value={fieldValue}
+          onChange={(e) =>
+            setFieldValue(e.target.value)
+          }
+          margin="normal"
+          fullWidth
+        />
+      </div>
+    );
+  }
+
+  // TODO: Handle enums (dropdowns)
+  // if (propertyValue.enum) {
+  //   return (
+  //     <FormControl key={fieldPath} fullWidth margin="normal">
+  //       <InputLabel>{propertyValue.title || key}</InputLabel>
+  //       <Select
+  //         value={fieldValue}
+  //         onChange={(e) =>
+  //           setFormData({ ...formData, [fieldPath]: e.target.value })
+  //         }
+  //         label={propertyValue.title || key}
+  //       >
+  //         {value.enum.map((option) => (
+  //           <MenuItem key={option} value={option}>
+  //             {option}
+  //           </MenuItem>
+  //         ))}
+  //       </Select>
+  //     </FormControl>
+  //   );
+  // }
+
+  // TODO: Handle numbers
+
+  // TODO: Handle booleans
+
+  // TODO: Handle objects
+  // TODO: Handle objects with additionalProperties
+  // TODO: Handle nested objects
+  // if (value.type === "object") {
+  //     return (
+  //       <div key={fieldPath} style={{ marginLeft: 20 }}>
+  //         <Typography variant="h6">{value.title || key}</Typography>
+  //         {generateFields(value, formData[key] || {}, setFormData, fieldPath)}
+  //       </div>
+  //     );
+  //   }
+
+  // TODO: Handle arrays
+  // TODO: Handle different field types within arrays
+  // if (value.type === "array") {
+  //     // This is a simplified version for arrays of strings or numbers.
+  //     // Complex array items would need more sophisticated handling.
+  //     return (
+  //       <div key={fieldPath}>
+  //         <InputLabel>{value.title || key}</InputLabel>
+  //         {formData[key] &&
+  //           formData[key].map((item, index) => (
+  //             <TextField
+  //               key={`${fieldPath}[${index}]`}
+  //               label={`${value.items.title || key} ${index + 1}`}
+  //               value={item}
+  //               onChange={(e) => {
+  //                 const newArray = [...formData[key]];
+  //                 newArray[index] = e.target.value;
+  //                 setFormData({ ...formData, [fieldPath]: newArray });
+  //               }}
+  //               margin="normal"
+  //               fullWidth
+  //             />
+  //           ))}
+  //         <Button
+  //           onClick={() =>
+  //             setFormData({
+  //               ...formData,
+  //               [fieldPath]: [...(formData[key] || []), ""],
+  //             })
+  //           }
+  //         >
+  //           Add {value.items.title || key}
+  //         </Button>
+  //       </div>
+  //     );
+  //   }
+};
+
+// Export the component.
+export default SchemaField;
---
+++ frontend/src/components/SchemaField.stories.js
@@ -0,0 +1,107 @@
+import SchemaField from "./SchemaField";
+import checkLink_v2 from "doc-detective-common/src/schemas/output_schemas/checkLink_v2.schema.json";
+import goTo_v2 from "doc-detective-common/src/schemas/output_schemas/goTo_v2.schema.json";
+import find_v2 from "doc-detective-common/src/schemas/output_schemas/find_v2.schema.json";
+import typeKeys_v2 from "doc-detective-common/src/schemas/output_schemas/typeKeys_v2.schema.json";
+import runShell_v2 from "doc-detective-common/src/schemas/output_schemas/runShell_v2.schema.json";
+import saveScreenshot_v2 from "doc-detective-common/src/schemas/output_schemas/saveScreenshot_v2.schema.json";
+import setVariables_v2 from "doc-detective-common/src/schemas/output_schemas/setVariables_v2.schema.json";
+import httpRequest_v2 from "doc-detective-common/src/schemas/output_schemas/httpRequest_v2.schema.json";
+import wait_v2 from "doc-detective-common/src/schemas/output_schemas/wait_v2.schema.json";
+import context_v2 from "doc-detective-common/src/schemas/output_schemas/context_v2.schema.json";
+import test_v2 from "doc-detective-common/src/schemas/output_schemas/test_v2.schema.json";
+import spec_v2 from "doc-detective-common/src/schemas/output_schemas/spec_v2.schema.json";
+import config_v2 from "doc-detective-common/src/schemas/output_schemas/config_v2.schema.json";
+
+// This default export determines where your story goes in the story list.
+export default {
+  title: "Doc Detective/SchemaField",
+  component: SchemaField,
+  // args at the component level for all stories.
+  args: {
+    schema: checkLink_v2,
+    pathToKey: "",
+    propertyKey: "id",
+    propertyValue: checkLink_v2.properties["id"],
+  },
+  argTypes: {
+    schema: {
+      options: [
+        "checkLink",
+        "goTo",
+        "find",
+        "typeKeys",
+        "runShell",
+        "saveScreenshot",
+        "setVariables",
+        "httpRequest",
+        "wait",
+        "context",
+        "test",
+        "spec",
+        "config",
+      ],
+      mapping: {
+        checkLink: checkLink_v2,
+        goTo: goTo_v2,
+        find: find_v2,
+        typeKeys: typeKeys_v2,
+        runShell: runShell_v2,
+        saveScreenshot: saveScreenshot_v2,
+        setVariables: setVariables_v2,
+        httpRequest: httpRequest_v2,
+        wait: wait_v2,
+        context: context_v2,
+        test: test_v2,
+        spec: spec_v2,
+        config: config_v2,
+      },
+    },
+    propertyKey: {
+        options: Object.keys(checkLink_v2.properties),
+    }
+  },
+};
+
+export const string = {
+  args: {
+    schema: checkLink_v2,
+    pathToKey: "",
+    propertyKey: "description",
+    propertyValue: checkLink_v2.properties["description"],
+  },
+  argTypes: {
+    schema: {
+      options: [
+        "checkLink",
+        "goTo",
+        "find",
+        "typeKeys",
+        "runShell",
+        "saveScreenshot",
+        "setVariables",
+        "httpRequest",
+        "wait",
+        "context",
+        "test",
+        "spec",
+        "config",
+      ],
+      mapping: {
+        checkLink: checkLink_v2,
+        goTo: goTo_v2,
+        find: find_v2,
+        typeKeys: typeKeys_v2,
+        runShell: runShell_v2,
+        saveScreenshot: saveScreenshot_v2,
+        setVariables: setVariables_v2,
+        httpRequest: httpRequest_v2,
+        wait: wait_v2,
+        context: context_v2,
+        test: test_v2,
+        spec: spec_v2,
+        config: config_v2,
+      },
+    },
+  }
+};
