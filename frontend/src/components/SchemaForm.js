cdc602dc477a5622007046ed45e3c464c81c6a9b
hawkeyexl <hawkeyexl@gmail.com>
2023-11-13 09:11:08 -0800

    Refactor SchemaForm component to use SchemaField and ReactMarkdown

--- frontend/src/components/SchemaForm.js
+++ frontend/src/components/SchemaForm.js
@@ -1,130 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import { TextField, Select, MenuItem, FormControl, InputLabel, Button, Checkbox, FormControlLabel, Typography } from '@mui/material';
-import Ajv from 'ajv';
+import React, { useState, useEffect } from "react";
+import ReactMarkdown from "react-markdown";
+import SchemaField from "./SchemaField";
 
-const ajv = new Ajv({ allErrors: true, useDefaults: true });
+const SchemaForm = ({ schema, passValueToParent }) => {
+  // Prop definitions.
+  // schema: The schema that the field belongs to.
+  // passValueToParent: A function that passes the value of the field to the parent component.
 
-const generateFields = (schema, formData, setFormData, path = '') => {
-  // The path is used to handle nested properties
-  return Object.entries(schema.properties).map(([key, value]) => {
-    const fieldPath = path ? `${path}.${key}` : key;
-    
-    // Handle strings
-    if (value.type === 'string' && !value.enum) {
-      return (
-        <TextField
-          key={fieldPath}
-          label={value.title || key}
-          value={formData[key] || ''}
-          onChange={(e) => setFormData({ ...formData, [fieldPath]: e.target.value })}
-          margin="normal"
-          fullWidth
-        />
-      );
-    }
-    
-    // Handle enums (dropdowns)
-    if (value.enum) {
-      return (
-        <FormControl key={fieldPath} fullWidth margin="normal">
-          <InputLabel>{value.title || key}</InputLabel>
-          <Select
-            value={formData[key] || ''}
-            onChange={(e) => setFormData({ ...formData, [fieldPath]: e.target.value })}
-            label={value.title || key}
-          >
-            {value.enum.map((option) => (
-              <MenuItem key={option} value={option}>
-                {option}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-      );
-    }
-    
-    // Handle nested objects
-    if (value.type === 'object') {
-      return (
-        <div key={fieldPath} style={{ marginLeft: 20 }}>
-          <Typography variant="h6">{value.title || key}</Typography>
-          {generateFields(value, formData[key] || {}, setFormData, fieldPath)}
-        </div>
-      );
-    }
-    
-    // Handle arrays
-    if (value.type === 'array') {
-      // This is a simplified version for arrays of strings or numbers.
-      // Complex array items would need more sophisticated handling.
-      return (
-        <div key={fieldPath}>
-          <InputLabel>{value.title || key}</InputLabel>
-          {formData[key] && formData[key].map((item, index) => (
-            <TextField
-              key={`${fieldPath}[${index}]`}
-              label={`${value.items.title || key} ${index + 1}`}
-              value={item}
-              onChange={(e) => {
-                const newArray = [...formData[key]];
-                newArray[index] = e.target.value;
-                setFormData({ ...formData, [fieldPath]: newArray });
-              }}
-              margin="normal"
-              fullWidth
-            />
-          ))}
-          <Button onClick={() => setFormData({ ...formData, [fieldPath]: [...(formData[key] || []), ''] })}>
-            Add {value.items.title || key}
-          </Button>
-        </div>
-      );
-    }
-    
-    // Add cases for other types as needed
-  });
-};
-
-const SchemaForm = ({ schema }) => {
-  const [formData, setFormData] = useState({});
-  const [errors, setErrors] = useState({});
-  
-  const validateFormData = (data) => {
-    const valid = ajv.validate(schema, data);
-    setErrors(valid ? {} : ajv.errors);
-    return valid;
+  // Set up state and handling.
+  const [formValue, setFormValue] = useState({});
+  const handleFormUpdate = (key, value) => {
+    // Update the form value.
+    setFormValue((prevState) => {
+      return { ...prevState, [key]: value };
+    });
   };
+  // Run custom logic.
+  // const text = isMultiline ? JSON.stringify(object, null, 2) : JSON.stringify(object);
 
-  useEffect(() => {
-    validateFormData(formData);
-  }, [formData]);
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (validateFormData(formData)) {
-      console.log('Form Data:', formData);
-    } else {
-      console.error('Validation Errors:', errors);
-    }
-  };
-
+  // Return the component.
   return (
-    <form onSubmit={handleSubmit}>
-      {generateFields(schema, formData, setFormData)}
-      <Button type="submit" color="primary" variant="contained" disabled={!Object.keys(errors).length === 0}>
-        Submit
-      </Button>
-      {Object.keys(errors).length > 0 && (
-        <div style={{ color: 'red' }}>
-          <ul>
-            {errors.map((error, index) => (
-              <li key={index}>{error.message}</li>
-            ))}
-          </ul>
-        </div>
-      )}
-    </form>
+    <div className="schema-form">
+      {schema.title && <ReactMarkdown>{`## ${schema.title}`}</ReactMarkdown>}
+      {schema.description && <ReactMarkdown>{schema.description}</ReactMarkdown>}
+      {Object.entries(schema.properties).map(([key, value]) => {
+        return (
+          <SchemaField
+            key={schema.title + key}
+            schema={schema}
+            propertyKey={key}
+            propertyValue={value}
+            passValueToParent={(value) => handleFormUpdate(key, value)}
+          />
+        );
+      })}
+    </div>
   );
 };
 
+// Default props.
+SchemaForm.defaultProps = {
+  passValueToParent: () => {},
+};
+
+// Export the component.
 export default SchemaForm;
