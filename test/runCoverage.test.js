e930622b53abc2872898eaa681b78ca4890fcaf6
hawkeyexl <hawkeyexl@gmail.com>
2024-06-23 13:43:04 -0700

    Updated relative linking

--- test/runCoverage.test.js
+++ test/runCoverage.test.js
@@ -1,7 +1,7 @@
-const { spawnCommand } = require("../src/utils");
-const assert = require("assert").strict;
 const path = require("path");
-const artifactPath = path.resolve("./test/artifacts");
+const { spawnCommand } = require(path.resolve(__dirname,"../src/utils"));
+const assert = require("assert").strict;
+const artifactPath = path.resolve(__dirname,"./artifacts");
 
 describe("Perform coverage analysis successfully", function () {
   // Set indefinite timeout
@@ -11,6 +11,10 @@ describe("Perform coverage analysis successfully", function () {
     // Find output file
     const outputFiles = coverateResults.stdout.split("See results at ");
     const outputFile = outputFiles[outputFiles.length - 1].trim();
+    // If output file is not found, throw an error
+    if (!outputFile) {
+      throw new Error(`Output file not found.\nOutput file: ${outputFile}\nCWD: ${process.cwd()}`);
+    }
     const result = require(outputFile);
     assert.equal(result.summary.covered, 6);
     assert.equal(result.summary.uncovered, 0);
