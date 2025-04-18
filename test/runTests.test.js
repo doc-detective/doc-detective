bdc32694936a38829b7cba9150e9b2fb21194e93
hawkeyexl <hawkeyexl@gmail.com>
2025-04-17 20:06:14 -0700

    Fix path resolution and correct spelling in test description
    - Update path resolution for spawnCommand to use a relative path
    - Correct spelling of "successfully" in the test description

--- test/runTests.test.js
+++ test/runTests.test.js
@@ -1,11 +1,11 @@
 const path = require("path");
-const { spawnCommand } = require(path.resolve(__dirname, "../src/utils"));
+const { spawnCommand } = require("../src/utils");
 const assert = require("assert").strict;
 const fs = require("fs");
 const artifactPath = path.resolve(__dirname, "./artifacts");
 const outputFile = path.resolve(`${artifactPath}/testResults.json`);
 
-describe("Run tests sucessfully", function () {
+describe("Run tests successfully", function () {
   // Set indefinite timeout
   this.timeout(0);
   it("All specs pass", async () => {
